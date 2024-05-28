c725639dd3d8e644cf643e2835e4e46c1bb922ac
DraconicOv <theredlionzzw@gmail.com>
2024-05-28 12:18:00 -0400

    added LCD constants

--- constants.h
+++ constants.h
@@ -4,5 +4,26 @@
 const int STRIP_SERVO_PIN = 9;
 const int ELECTRODE_SERVO_PIN = 10;
 
+/**
+ * @brief The pin number for the lens LED.
+ *
+ * This constant represents the pin number used to control the lens LED.
+ * It is set to 11, and the `analogWrite()` function can be used to control the LED brightness.
+ */
+const int LENS_LED_PIN = 11;
+/**
+ * @brief The pin number for the bottom LED.
+ *
+ * This constant represents the pin number used to control the lens LED.
+ * It is set to 12, and the `analogWrite()` function can be used to control the LED brightness.
+ */
+const int BOTTOM_LED_PIN = 12;
+/**
+ * @brief The pin number for the side LED.
+ *
+ * This constant represents the pin number used to control the lens LED.
+ * It is set to 13, and the `analogWrite()` function can be used to control the LED brightness.
+ */
+const int SIDE_LED_PIN = 13;
 
 #endif
--- mainBAMSI.ino
+++ mainBAMSI.ino
@@ -1,10 +1,17 @@
+#include <LiquidCrystal_I2C.h>
 #include <Adafruit_MAX31865.h>
 #include <Servo.h>
 #include <constants.h>
 
 Servo stripMotor;
-Servo electrodeLiftServo;
-
+Servo electrodeLiftServo; 
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Address, cols, rows //TODO: Double check address
+// tell the screen to write on the top row
+// lcd.setCursor(0,0);
+// tell the screen to write “hello, from” on the top  row
+// lcd.print(“Hello, From”);  
+// tell the screen to write on the bottom  row
+// lcd.setCursor(0,1);
 
 void setup() {
   stripMotor.attach(STRIP_SERVO_PIN);
@@ -12,5 +19,5 @@ void setup() {
 }
 
 void loop() {
-  
+  // stripMotor.write();
 }
